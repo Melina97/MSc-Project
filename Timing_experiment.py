cd01036563f6a3275c27e6c069d828ef4835ca2b
Melina Giagiozis <melina.giagiozis20@imperial.ac.uk>
2021-06-09 10:50:11 +0100

    corrections

--- Timing_experiment.py
+++ Timing_experiment.py
@@ -5,7 +5,7 @@ from DG_advection_equation_with_upwinding import DG_advection_timing
 
 # Run function without annotating tape (context manager)
 with stop_annotating():
-    DG_advection_timing(40, 40, 600.0, options={"ksp_max_it": 100, "ksp_type": "cg"})
+    DG_advection_timing(x_coords = 40, y_coords = 40, time_steps = 600.0, options={"ksp_max_it": 100, "ksp_type": "cg"})
 
 # Timing experiment
-DG_advection_timing(40, 40, 600.0, print_times=True, options={"ksp_max_it": 100, "ksp_type": "cg"})
+DG_advection_timing(x_coords = 40, y_coords = 40, time_steps = 600.0, print_times=True, options={"ksp_max_it": 100, "ksp_type": "cg"})
