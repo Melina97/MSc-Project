aa9698d40dc93554ee2e60e07e813cf68c54f6f8
Melina Giagiozis <melina.giagiozis20@imperial.ac.uk>
2021-06-09 10:50:39 +0100

    corrections

--- Tape.py
+++ Tape.py
@@ -3,7 +3,7 @@ from firedrake import *
 from firedrake_adjoint import *
 from DG_advection_equation_with_upwinding import DG_advection_solve
 
-DG_advection_solve(40, 40, 5.0, options={"ksp_max_it": 100, "ksp_type": "cg"})
+DG_advection_solve(x_coords = 40, y_coords = 40, time_steps = 5.0, options={"ksp_max_it": 100, "ksp_type": "cg"})
 
 tape = get_working_tape()
-tape.visualise(output='tape.dot', launch_tensorboard=True, open_in_browser=False)
+tape.visualise(output='tape.dot', launch_tensorboard=False, open_in_browser=True)
