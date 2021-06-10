e89fabfc167819a6f1c162487ed273767595cc32
Melina Giagiozis <melina.giagiozis20@imperial.ac.uk>
2021-06-10 12:48:40 +0100

    corrections

--- Timing_experiment.py
+++ Timing_experiment.py
@@ -9,3 +9,11 @@ with stop_annotating():
 
 # Timing experiment
 DG_advection_timing(x_coords = 40, y_coords = 40, time_steps = 600.0, print_times=True, options={"ksp_max_it": 100, "ksp_type": "cg"})
+
+
+"""
+Last Run 9th June 12:46:
+The taping takes: 10.799703121185303 seconds
+Running the tape takes: 67.61313724517822 seconds
+Computing the derivative of the reduced functional takes: 138.66187810897827 seconds
+"""
