61db8f835c52ae7a0220594d45fc4e712c1baeb2
Melina Giagiozis <melina.giagiozis20@imperial.ac.uk>
2021-07-01 14:22:59 +0100

    small changes

--- Tape.py
+++ Tape.py
@@ -6,4 +6,4 @@ from DG_advection_equation_with_upwinding import DG_advection_solve
 DG_advection_solve(x_coords = 40, y_coords = 40, time_steps = 5.0, options={"ksp_max_it": 100, "ksp_type": "cg"})
 
 tape = get_working_tape()
-tape.visualise(output='tape.dot', launch_tensorboard=False, open_in_browser=True)
+tape.visualise(output='tape_x.dot', launch_tensorboard=False, open_in_browser=True)
