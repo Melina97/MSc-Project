98645717076d08ba09ee1e3a9851290cb715732f
Melina Giagiozis <melina.giagiozis20@imperial.ac.uk>
2021-07-01 14:22:30 +0100

    small changes

--- Timing_experiment.py
+++ Timing_experiment.py
@@ -10,7 +10,6 @@ with stop_annotating():
 # Timing experiment
 DG_advection_timing(x_coords = 40, y_coords = 40, time_steps = 600.0, print_times=True, options={"ksp_max_it": 100, "ksp_type": "cg"})
 
-
 """
 Last Run 10th June 12:46:
 The taping takes: 10.799703121185303 seconds
