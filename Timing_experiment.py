51a6e0540084d5f5b06532edab4c7bd4ea3c4b79
Melina Giagiozis <melina.giagiozis20@imperial.ac.uk>
2021-07-08 16:38:27 +0100

    added context manager

--- Timing_experiment.py
+++ Timing_experiment.py
@@ -1,14 +1,20 @@
 
 from firedrake import *
 from firedrake_adjoint import *
-from DG_advection_equation_with_upwinding import DG_advection_timing
+from DG_advection_equation_with_upwinding import DG_advection_solve, DG_advection_timing
 
 # Run function without annotating tape (context manager)
-with stop_annotating():
-    DG_advection_timing(x_coords = 40, y_coords = 40, time_steps = 600.0, options={"ksp_max_it": 100, "ksp_type": "cg"})
+with PETSc.Log.Stage("Cache warmup"):
+    with PETSc.Log.Event("Cache warmup"):
+        DG_advection_solve(x_coords = 40, y_coords = 40, time_steps = 10.0, options={"ksp_max_it": 100, "ksp_type": "cg"})
+
+tape = get_working_tape()
+tape.clear_tape()
 
 # Timing experiment
-DG_advection_timing(x_coords = 40, y_coords = 40, time_steps = 600.0, print_times=True, options={"ksp_max_it": 100, "ksp_type": "cg"})
+with PETSc.Log.Stage("Timing run"):
+    with PETSc.Log.Event("Timing run"):
+        DG_advection_timing(x_coords = 40, y_coords = 40, time_steps = 10.0, print_times=True, options={"ksp_max_it": 100, "ksp_type": "cg"})
 
 """
 Last Run 10th June 12:46:
