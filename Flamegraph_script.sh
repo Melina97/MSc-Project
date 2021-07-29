97153b6e504c7c11419d3406ff3ecde76613736a
Melina Giagiozis <melina.giagiozis20@imperial.ac.uk>
2021-07-29 12:41:20 +0100

    Updated

--- Flamegraph_script.sh
+++ Flamegraph_script.sh
@@ -10,24 +10,26 @@ export OMP_NUM_THREADS=1
 cd ~/firedrake/src/firedrake 
 git checkout Melina97/before_changes
 cd ~/firedrake/src/PyOP2
-git checkout Melina97/before_changes
+git checkout master
+#git checkout Melina97/before_changes
 
 # enter MSc repo
 cd ~/Desktop/MScProject/Code/MSc-Project
 
 # make flame graph for master branch
-python Timing_experiment.py -log_view :flamegraph_without_cache.txt:ascii_flamegraph
-./flamegraph.pl --title="Flame Graph: without the cache fix" --countname="microseconds" flamegraph_without_cache.txt > flamegraph_without_cache.svg
+python Timing_experiment.py -log_view :flamegraph_without_changes.txt:ascii_flamegraph
+./flamegraph.pl --title="Flame Graph: before changes" --countname="microseconds" flamegraph_without_changes.txt > flamegraph_without_changes.svg
 
 # checkout Melina97/cache_assign branch and Melina97/fix_cache_problem branch
 cd ~/firedrake/src/firedrake
 git checkout Melina97/after_changes
 cd ~/firedrake/src/PyOP2
-git checkout Melina97/after_changes
+git checkout master
+#git checkout Melina97/after_changes
 
 # enter MSc repo
 cd ~/Desktop/MScProject/Code/MSc-Project
 
 # make flame graph for Melina97/cache_assign branch
-python Timing_experiment.py -log_view :flamegraph_with_cache.txt:ascii_flamegraph
-./flamegraph.pl --title="Flame Graph: with the cache fix" --countname="microseconds" flamegraph_with_cache.txt > flamegraph_with_cache.svg
+python Timing_experiment.py -log_view :flamegraph_with_changes.txt:ascii_flamegraph
+./flamegraph.pl --title="Flame Graph: after changes" --countname="microseconds" flamegraph_with_changes.txt > flamegraph_with_changes.svg
