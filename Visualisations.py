634647ed0ff49991fa9f824cef79c8818728c118
Melina Giagiozis <melina.giagiozis20@imperial.ac.uk>
2021-08-03 09:19:40 +0100

    Shorter tape

--- Visualisations.py
+++ Visualisations.py
@@ -5,25 +5,19 @@ from firedrake_adjoint import *
 # create AdjFloat
 
 a = AdjFloat(2)
-b = AdjFloat(3)
-c = AdjFloat(27)
-d = AdjFloat(3.14)
-e = AdjFloat(999)
-f = AdjFloat(0.001)
-g = AdjFloat(7.54)
-h = AdjFloat(70)
+b = AdjFloat(27)
+e = AdjFloat(0.001)
+f = AdjFloat(3.14)
+g = AdjFloat(70)
 
 #operations
 
-i = a+b
-j = c*h
-k = j/h
-l = e/d
-m = f-g
-n = i**(a/l)
+j = b*g
+k = g**(1/a)
+m = e+f
 o = m-k
-p = h/j
-q = (n+p)**o
+p = g/j
+q = o-p
 
 tape = get_working_tape()
 tape.visualise(output='test1.dot', launch_tensorboard=False, open_in_browser=True)
